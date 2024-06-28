6175e3db9248550d81f3527653c9bd970518316d
Mohit251103 <142974436+Mohit251103@users.noreply.github.com>
2024-06-28 11:32:33 +0530

    Manual authentication implemented

--- Server/src/lib/send-mail.js
+++ Server/src/lib/send-mail.js
@@ -1,3 +1,27 @@
 const nodemailer = require("nodemailer")
 
-const transporter = nodemailer.createTransport()
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const transporter = nodemailer.createTransport({
+    service:"gmail",
+    host: 'smtp.gmail.com',
+   port: 465,
+   secure: true,
+    auth:{
+        user:process.env.EMAIL,
+        pass:process.env.EMAIL_PASS
+    }
+})
+
+const sendMail = async ({email,otp})=>{
+    const info = await transporter.sendMail({
+        from:process.env.EMAIL,
+        to:email,
+        subject:"OTP for user verification",
+        text:`Your OTP is ${otp}`
+    })
+
+    console.log(`Mail sent successfully ${info.messageId}`);
+}
+
+module.exports = sendMail;
--- Server/src/routes/user-auth.js
+++ Server/src/routes/user-auth.js
@@ -2,28 +2,87 @@ const router = require("express").Router();
 const User = require("../modules/user-model.js");
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
+const sendMail = require("../lib/send-mail.js");
+const generateOtp = require("../lib/generateOtp.js");
 
-router.post("/", async (req,res)=>{
+router.post("/signup", async (req,res)=>{
     try {
         const reqBody = req.body();
 
         const user = await User.findOne({email:reqBody.email});
-        if(!user){
-            return res.send({error:"User already present",success:false,status:400});
+        if(user){
+            return res.status(400).json({error:"User already present",success:false});
         }
         
         const hashedPassword = await bcrypt.hash(reqBody.password,10);
-        const userData = {username:reqBody.username, email:reqBody.email, password:hashedPassword}
+        const otp = generateOtp();
+        const userData = {username:reqBody.username, email:reqBody.email, password:hashedPassword, isverifiedtoken:otp};
         await User.create(userData);
+
+        sendMail({email:reqBody.email, otp});
+        return res.status(200).json({message:"Otp has been sent to email for verification",success:true});
         
+    } catch (error) {
+        res.status(500).json({error,message:"Internal server error"});
+        res.redirect(`${process.env.VITE_APP_URL}/servererror`);
+    }
+})
+
+router.post("/verify", async (req,res)=>{
+    try {
+        const {email,otp} = req.body;
+        const user = await User.findOne({email});
+
+        if(otp!==this.user.otp){
+            return res.status(400).json({error:"Incorrect OTP",success:false});
+        }
+        return res.status(200).json({message:"Verification successfull",success:true});
 
     } catch (error) {
-        
+        res.status(500).json({error,message:"Internal server error"});
+        res.redirect(`${process.env.VITE_APP_URL}/servererror`);
+    }
+})
+
+router.patch("/verify/resend",async (req,res)=>{
+    try {
+        const {email} = req.body;
+        const otp = generateOtp();
+        await User.findOneAndUpdate({email},{isverifiedtoken:otp});
+        sendMail({email,otp});
+        return res.status(200).json({message:"Otp resended to email",success:true});
+    } catch (error) {
+        res.status(500).json({error,message:"Internal server error"});
+        res.redirect(`${process.env.VITE_APP_URL}/servererror`);
+    }
+})
+
+router.post("/login", async(req,res)=>{
+    try {
+        const {email,password} = req.body();
+
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"User does not exist",success:false});
+        }
+
+        const isValidPass = bcrypt.compare(password,user.password);
+        if(!isValidPass){
+            return res.status(401).json({error:"Invalid credentials",success:false});
+        }
+
+        // res.cookie("id",user.id,{maxAge:60*60*24*1000, signed:true, httpOnly:true})
+        req.session.userId = user.id;
+        res.status(200).json({message:"User successfully logged in"});
+
+    } catch (error) {
+        res.status(500).json({error,message:"Internal server error"});
+        res.redirect(`${process.env.VITE_APP_URL}/servererror`);
     }
 })
 
 
-// TODO : manual method to be implemented and Oauth to be tested
+// TODO : Oauth to be tested
 
 router.get('/status',(req,res)=>{
     if(req.user){
